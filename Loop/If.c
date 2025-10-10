ab5af6d30c9b58f96c7bd457ae0173cd51e8ce81
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-10-10 18:39:09 -0400

    Loop Folder

---
+++ Loop/If.c
@@ -0,0 +1,15 @@
+//To check if a is equal to b
+
+#include<stdio.h>
+int main() {
+
+    int a,b;
+    printf("Enter value of a");
+    scanf("%d",&a);
+    printf("Enter value of b");
+    scanf("%d",&b);
+    if(a==b) {
+        printf("a is equal to b");
+    }
+    return 0;
+}
---
+++ Loop/README.md
@@ -0,0 +1,7 @@
+# This folder contains loop related concepts of C Programming Language
+
+# Topics included
+- if statement
+- else-if statement
+- nested else-if statement
+ if-elseif ladder
