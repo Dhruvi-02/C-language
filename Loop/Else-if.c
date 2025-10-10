cddf3f8b1f405cacf7e32aa8507d04dc57a52ae6
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-10-10 18:46:45 -0400

    Else-if

---
+++ Loop/Else-if.c
@@ -0,0 +1,17 @@
+//To check if number is odd or even 
+
+#include<stdio.h>
+
+int main() {
+ 
+    int num;
+    printf("Enter a number");
+    scanf("%d",&num);
+    if(num%2==0) {
+        printf("The number is even");
+    }
+    else {
+        printf("The number is odd");
+    }
+    return 0;
+}
