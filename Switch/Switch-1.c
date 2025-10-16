81e698971a1d58bb7cd866b101ce6d9c5f9076f3
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-10-16 17:06:23 -0400

    Modified Switch Folder

---
+++ Switch/Switch-1.c
@@ -0,0 +1,23 @@
+//
+
+#include<stdio.h>
+
+int main() {
+
+    char grade='B';
+    switch (grade)
+    {
+    case 'A': printf("Distinction");
+        break;
+    case 'B': printf("First Class");
+        break;
+    case 'C': printf("Second Class");
+        break;
+    case 'D': printf("Pass");
+        break;
+    
+    default: printf("Fail");
+        break;
+    }
+    return 0;
+}
