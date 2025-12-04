39e87d69738d0cbace6d51f88b62f4a09a3f369f
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-03 20:24:11 -0500

    Example of while loop

---
+++ Looping-Statements.c/while.c
@@ -0,0 +1,20 @@
+//To evaluate the equation x^n
+
+#include<stdio.h>
+
+int main() {
+    float x,p=1;
+    int n;
+
+    printf("Enter the value of x:");
+    scanf("%f",&x);
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+
+    int count = 1;
+    while(count <= n) {
+        p = p*x;
+        count ++;
+    }
+    printf("\nx = %f \nn = %d \nx to power n = %f\n",x,n,p);
+}
