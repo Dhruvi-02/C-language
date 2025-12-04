401fbad76884e2296a23a1eaa9a88847fa47cc1b
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-03 21:57:25 -0500

    Adding example of Conditional Statement

---
+++ Conditional-Statements.c/Problem-1.c
@@ -0,0 +1,28 @@
+//To check whether student is pass or fail based on average of four subject marks
+
+#include<stdio.h>
+
+int main() {
+
+    int m1,m2,m3,m4,sum,avg;
+    printf("Enter the marks of subject-1: ");
+    scanf("%d",&m1);
+    printf("Enter the marks of subject-2: ");
+    scanf("%d",&m2);
+    printf("Enter the marks of subject-3: ");
+    scanf("%d",&m3);
+    printf("Enter the marks of subject-4: ");
+    scanf("%d",&m4);
+
+    sum = m1+m2+m3+m4;
+    avg = sum/4;
+
+    if(avg>25) {
+        printf("Student pass in the exam");
+    }
+    else {
+        printf("Student fail in the exam");
+    }
+
+    return 0;
+}
