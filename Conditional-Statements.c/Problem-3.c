744bc3c678dc06251b48a35187fda47a9e04051d
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-04 13:57:28 -0500

    Fixed the error in condition

--- Conditional-Statements.c/Problem-3.c
+++ Conditional-Statements.c/Problem-3.c
@@ -8,21 +8,21 @@ int main() {
     printf("Enter your marks: ");
     scanf("%d",&marks);
 
-    if(marks>='80' && marks<='100') {
+    if(marks>=80 && marks<=100) {
         printf("Grade: Distinction");
     }
-    else if(marks>='60' && marks<='79') {
+    else if(marks>=60 && marks<=79) {
         printf("Grade: First Class");
     }
-    else if(marks>='35' && marks<='59') {
+    else if(marks>=35 && marks<=59) {
         printf("Grade: Second Class");
     }
-    else if(marks>='0' && marks<='34') {
+    else if(marks>=0 && marks<=34) {
         printf("Grade: Fail");
     }
     else {
         printf("Invalid marks");
     }
-    
+
     return 0;
 }
