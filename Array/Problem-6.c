6cd9b0622dbb81796525b94968b306ce2f817200
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-09 22:03:59 -0500

    Problem-6

---
+++ Array/Problem-6.c
@@ -0,0 +1,34 @@
+//Program to delete value at ith location or at location entered by the user using one dimensional array
+
+#include<stdio.h>
+
+int main() {
+
+    int N,i,idx,val;
+    printf("Enter the length of array: ");
+    scanf("%d",&N);
+
+    int a[N];
+
+    printf("Enter the elements of array:\n");
+    for(i=0;i<N;i++) {
+        scanf("%d",&a[i]);
+    }
+
+    printf("\nThe array is:\t");
+    for(i=0;i<N;i++) {
+        printf("%d\t",a[i]);
+    }
+
+    printf("\nEnter the ith location where value is to be removed: ");
+    scanf("%d",&idx);
+
+    a[idx] = 0;
+
+    printf("\nThe new array is: ");
+    for(i=0;i<N;i++) {
+        printf("%d\t",a[i]);
+    }
+
+    return 0;
+}
