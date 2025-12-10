c1b594b41b53ed52c904de5048862aea6dd7842e
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-09 21:59:19 -0500

    Problem-5

---
+++ Array/Problem-5.c
@@ -0,0 +1,36 @@
+//Program to insert value at ith location or at location entered by user using one dimensional array
+
+#include<stdio.h>
+
+int main() {
+
+    int N,i,idx,val;
+    printf("Enter the length of array: ");
+    scanf("%d",&N);
+
+    int a[N];
+
+    printf("Enter the elements of array:\n");
+    for(i=0;i<N;i++) {
+        scanf("%d",&a[i]);
+    }
+
+    printf("\nThe array is:\t");
+    for(i=0;i<N;i++) {
+        printf("%d\t",a[i]);
+    }
+
+    printf("\nEnter the ith location: ");
+    scanf("%d",&idx);
+    printf("\nEnter the value to be inserted: ");
+    scanf("%d",&val);
+
+    a[idx] = val;
+
+    printf("\nThe new array is: ");
+    for(i=0;i<N;i++) {
+        printf("%d\t",a[i]);
+    }
+
+    return 0;
+}
