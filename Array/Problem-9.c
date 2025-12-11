c1ab96ffc1e834ad50e84bba2aa913b5829417fb
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-10 21:46:15 -0500

    Fixing typo in printf statement

--- Array/Problem-9.c
+++ Array/Problem-9.c
@@ -5,13 +5,13 @@
 int main() {
     
     int m1,n1,m2,n2,i,j,k;
-    printf("Enter the no. of rows of array-1: ");
+    printf("Enter the no. of rows of matrix-1: ");
     scanf("%d",&m1);
-    printf("Enter the no. of columns of array-1: ");
+    printf("Enter the no. of columns of matrix-1: ");
     scanf("%d",&n1);
-    printf("Enter the no. of rows of array-2: ");
+    printf("Enter the no. of rows of matrix-2: ");
     scanf("%d",&m2);
-    printf("Enter the no. of columns of array-2: ");
+    printf("Enter the no. of columns of matrix-2: ");
     scanf("%d",&n2);
 
     if(n1!=m2) {
@@ -30,21 +30,21 @@ int main() {
 
     int a[10][10],b[10][10],c[10][10];
 
-    printf("\nEnter elements of array-1:\n");
+    printf("\nEnter elements of matrix-1:\n");
     for(i=0;i<m1;i++) {
         for(j=0;j<n1;j++) {
             scanf("%d",&a[i][j]);
         }
     }
 
-    printf("\nEnter elements of array-2:\n");
+    printf("\nEnter elements of matrix-2:\n");
     for(i=0;i<m2;i++) {
         for(j=0;j<n2;j++) {
             scanf("%d",&b[i][j]);
         }
     }
 
-    printf("\nThe array-1 is:\n");
+    printf("\nThe matrix-1 is:\n");
     for(i=0;i<m1;i++) {
         for(j=0;j<n1;j++) {
             printf("%d\t",a[i][j]);
@@ -52,7 +52,7 @@ int main() {
         printf("\n");
     }
 
-    printf("\nThe array-2 is:\n");
+    printf("\nThe matrix-2 is:\n");
     for(i=0;i<m2;i++) {
         for(j=0;j<n2;j++) {
             printf("%d\t",b[i][j]);
