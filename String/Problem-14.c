4a17fd895bd75b7b6aca897b5510099940814be5
Dhruvi-02 <dhruvi018patel@gmail.com>
2025-12-22 21:44:07 -0500

    Toggling of string

---
+++ String/Problem-14.c
@@ -0,0 +1,31 @@
+//Program to convert the string into uppercase
+
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+int main() {
+
+    char str[100],temp[100];
+    int i,len;
+
+    printf("Enter the string: ");
+    gets(str);
+
+    len = strlen(str);
+
+    for(i=0;i<len;i++) {
+        if(islower(str[i])) {
+            temp[i] = toupper(str[i]);
+        }
+        else {
+            temp[i] = str[i];
+        }
+    }
+
+    temp[i] = '\0';
+
+    printf("The uppercase string is: %s",temp);
+
+    return 0;
+}
